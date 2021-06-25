ad9d391e4a9f4924aac68df69d9bcdf1b8994f18
reman131 <reman235711@gmail.com>
2021-06-26 01:07:05 +0200

    fourth commit, add user and role models

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,6 @@
 const dbConfig = require("../config/db.config.js");
 
+// set up database connection
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
@@ -20,6 +21,34 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+// define database modules.
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.user = require("./user.model.js")(sequelize,Sequelize);
+db.role = require("./role.model.js")(sequelize,Sequelize);
+
+// define the relation between User table and Roles table.
+
+/*
+db.user.belongsToMany(db.role,{
+  through:"user_role",
+  foreignKey:"userId",
+  otherKey:"roleId"
+});
+
+db.role.belongsToMany(db.user,{
+  through:"user_role",
+  foreignKey:"roleId",
+  otherKey:"userId"
+});
+*/
+//faster
+db.user.belongsToMany(db.role,{through:"user-role"});
+db.role.belongsToMany(db.user,{through:"user-role"});
+
+// 
+db.ROLES = ["user", "admin", "moderator"];
+
+console.log("------------------------- koshi - out models/index --------------------------");
+
 
 module.exports = db;
